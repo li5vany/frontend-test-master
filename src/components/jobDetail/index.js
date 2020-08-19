79e84fac6f2fbe43fe5d65b6064e517058d78598
Lisvany Alonso <7i5vany@gmail.com>
2020-08-19 18:33:33 -0400

    fixing the filter box style

---
+++ src/components/jobDetail/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import moment from "moment";
+import './jobItem.scss'
+
+const JobDetail = ({job}) => {
+
+  return (
+    <div className="job-item">
+      <img src={job.company_logo} alt={job.company} height="100"/>
+      <div className="company-description">
+        <div><span className="company-name">{job.company}</span> {job.type && <span className="new">{job.type}</span>} {job.new && <span className="featured">featured</span>}</div>
+        <h3 className="position">{job.title}</h3>
+        <div className="secondary-color detail">about {moment(job.created_at).fromNow()} <span className="dot"/> {job.location}</div>
+
+        <div className="separator"/>
+
+        <div className="tags">
+          <div dangerouslySetInnerHTML={{__html: job.description}}/>
+        </div>
+
+        <div dangerouslySetInnerHTML={{__html: job.how_to_apply}} style={{overflow: "auto"}}/>
+
+      </div>
+    </div>
+  )
+};
+
+export default JobDetail;
